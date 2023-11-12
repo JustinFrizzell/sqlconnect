a48d249d42203dddd93fb66b804464c56c2e8ead
Justin Frizzell <143661894+JustinFrizzell@users.noreply.github.com>
2023-11-12 10:52:55 +0000

    Updated functions used in example usage

--- example_usage.py
+++ example_usage.py
@@ -1,6 +1,10 @@
 import SQLconnect as sc
 
+# Establish a connection to the database
 connection = sc.SQLconnector("WorldWideImporters")
-df = connection.sql_query("query.sql")
 
-print(df.head())
+# Assign the results of a query to a pandas DataFrame
+df = connection.query_to_df("query.sql")
+
+# Print the top 5 rows
+print(df.head(5))
