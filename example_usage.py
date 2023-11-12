277c7043deffe53c331889c0881ff8954a2054f1
Justin Frizzell <143661894+JustinFrizzell@users.noreply.github.com>
2023-11-12 13:50:30 +0000

    Added example properties

--- example_usage.py
+++ example_usage.py
@@ -8,3 +8,7 @@ df = connection.query_to_df("query.sql")
 
 # Print the top 5 rows
 print(df.head(5))
+
+# Print the connection details
+print(f"connection_name: {connection.connection_name}")
+print(f"database_url: {connection.database_url}")
