6f09426bacefd3ec6163d982d6c6f6b7a34899d7
Justin Frizzell <143661894+JustinFrizzell@users.noreply.github.com>
2023-11-25 14:37:47 +0000

    Check project root before home directory

--- SQLconnect/config.py
+++ SQLconnect/config.py
@@ -8,13 +8,28 @@ from dotenv import load_dotenv
 def get_connection_config(connection_name: str, config_path: str = None) -> dict:
     """Using pathlib to read the YAML file."""
 
-    if config_path is not None:
-        config_text = Path(config_path).read_text(encoding="utf-8")
-    else:
-        config_text = Path("connections.yaml").read_text(encoding="utf-8")
+    # List of potential paths for the configuration file
+    config_paths = (
+        [Path(config_path)]
+        if config_path
+        else [
+            Path("connections.yaml"),
+            Path("connections.yml"),
+            Path.home() / "connections.yaml",
+            Path.home() / "connections.yml",
+        ]
+    )
 
-    config = yaml.safe_load(config_text)
-    return config["connections"][connection_name]
+    for path in config_paths:
+        if path.exists():
+            config_text = path.read_text(encoding="utf-8")
+            config = yaml.safe_load(config_text)
+            return config["connections"][connection_name]
+
+    raise FileNotFoundError(
+        f"Config file not found in {Path('connections.yaml').absolute()} "
+        f"or {Path.home() / 'connections.yaml'}"
+    )
 
 
 def get_db_url(connection_config: dict) -> str:
@@ -29,9 +44,24 @@ def get_db_url(connection_config: dict) -> str:
     # Check for username and password
     auth_details = ""
     if "username" in connection_config and "password" in connection_config:
-        load_dotenv()
-        username = os.getenv(connection_config["username"].strip("${}"))
-        password = os.getenv(connection_config["password"].strip("${}"))
+        if Path(".env").exists():
+            load_dotenv(Path(".env"))
+        else:
+            home_env_path = Path.home() / ".env"
+            if home_env_path.exists():
+                load_dotenv(home_env_path)
+
+        env_username_key = connection_config["username"].strip("${}")
+        env_password_key = connection_config["password"].strip("${}")
+        username = os.getenv(env_username_key)
+        password = os.getenv(env_password_key)
+
+        if username is None or password is None:
+            raise EnvironmentError(
+                f"Environment variables '{env_username_key}' and/or '{env_password_key}' not "
+                f"found in {Path(".env").absolute()} or {Path.home() / ".env"}"
+            )
+
         auth_details = f"{username}:{password}@"
 
     # Construct the connection string
