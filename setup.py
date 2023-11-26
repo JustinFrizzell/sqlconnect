fec7ee9bc7869cd7586ce3553ce1907d4c68ed1a
Justin Frizzell <143661894+JustinFrizzell@users.noreply.github.com>
2023-11-26 19:51:38 +0000

    Added documentation

--- setup.py
+++ setup.py
@@ -3,12 +3,17 @@ from pathlib import Path
 
 setuptools.setup(
     name="SQLconnect",
-    version="0.1.1",
+    version="0.2.0",
     author="Justin Frizzell",
-    description=""" Package to simplify connections to SQL databases. """,
-    long_description=Path("README.md").read_text(encoding="utf=8"),
+    description="Package to simplify connections to SQL databases.",
+    long_description=Path("README.md").read_text(encoding="utf-8"),
     long_description_content_type="text/markdown",
     url="https://github.com/JustinFrizzell/SQLconnect",
+    project_urls={
+        "Documentation": "https://sqlconnect.readthedocs.io/en/latest/",
+        "Source": "https://github.com/JustinFrizzell/SQLconnect",
+        # You can add other links here if necessary
+    },
     packages=setuptools.find_packages(),
     install_requires=["pandas", "sqlalchemy", "pyyaml", "pyodbc", "python-dotenv"],
     classifiers=[
