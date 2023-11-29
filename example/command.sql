1aa8e520dd2b8ba25f1df11ebc795c459f1516a9
Justin Frizzell <143661894+JustinFrizzell@users.noreply.github.com>
2023-11-29 18:32:57 +0000

    Removed old examples

--- example/command.sql
+++
@@ -1,5 +0,0 @@
-EXEC [dbo].[InsertEventLog]
-    @SubjectArea = N'Finance',
-    @ProcessName = N'QuarterlyReportGeneration',
-    @InstanceID = N'Q3-2023',
-    @EventData = '<EventData><Detail>Report generated successfully</Detail></EventData>'
--- example/query.sql
+++
@@ -1,1 +0,0 @@
-SELECT TOP (1000) * FROM [Sales].[Invoices]
