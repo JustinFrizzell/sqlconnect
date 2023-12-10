875176809b931bd074a4ed650e555eb7fff6aa56
Justin Frizzell <143661894+JustinFrizzell@users.noreply.github.com>
2023-12-10 13:44:23 +0000

    Added migration

--- tests/integration/docker-entrypoint.sh
+++ tests/integration/docker-entrypoint.sh
@@ -3,5 +3,8 @@
 echo "Waiting for Postgres to start"
 ./tests/integration/wait-for-it.sh postgres_db:5432 
 
+echo "Migrating database"
+python ./tests/integration/migration.py
+
 echo "Starting pytest"
 python -m pytest
