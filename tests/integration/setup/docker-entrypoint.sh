254747302fb43f2f1ad4c9a0c6509fc77598f11f
Justin Frizzell <143661894+JustinFrizzell@users.noreply.github.com>
2023-12-30 19:14:38 +0000

    Update header

--- tests/integration/setup/docker-entrypoint.sh
+++ tests/integration/setup/docker-entrypoint.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-# This script is used to set up the environment for the integration tests
+# This script is used to set up the environment for the CI tests
 # 1. Wait for database services to be ready to accept connections
 # 2. Migrate the databases to provide data for testing
 # 3. Initiate pytest tests
+# 4. Check python code is formatted with Black
 
 echo "Waiting for Postgres to start"
 ./tests/integration/setup/wait-for-it.sh postgres_db:5432 
