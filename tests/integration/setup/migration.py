6bfdd48a257cf56f41f1cc13ef3bd67b3203446c
Justin Frizzell <143661894+JustinFrizzell@users.noreply.github.com>
2023-12-30 19:14:28 +0000

    Refactor migration

--- tests/integration/setup/migration.py
+++ tests/integration/setup/migration.py
@@ -1,71 +1,41 @@
 from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData
 from sqlalchemy.exc import SQLAlchemyError
 
-# Database credentials and connection details
-DATABASE_URL = (
-    "postgresql+psycopg2://postgres:mysecretpassword@postgres_db:5432/postgres"
-)
-engine = create_engine(DATABASE_URL)
-metadata = MetaData()
 
-# Define the table
-employees = Table(
-    "employees",
-    metadata,
-    Column("id", Integer, primary_key=True),
-    Column("name", String),
-    Column("position", String),
-    Column("database_url", String),
-)
+def create_table_and_insert_data(database_url):
+    engine = create_engine(database_url)
+    metadata = MetaData()
 
-# Create the table in the database
-metadata.create_all(engine)
+    employees = Table(
+        "employees",
+        metadata,
+        Column("id", Integer, primary_key=True),
+        Column("name", String),
+        Column("position", String),
+        Column("database_url", String),
+    )
 
-try:
-    with engine.connect() as connection:
-        # Insert data
-        data_to_insert = {
-            "id": 1,
-            "name": "Jane Doe",
-            "position": "Data Engineer",
-            "database_url": DATABASE_URL,
-        }
-        connection.execute(employees.insert(), data_to_insert)
-        connection.commit()
-        print("Data inserted successfully.")
-except SQLAlchemyError as e:
-    print(f"An error occurred: {e}")
+    metadata.create_all(engine)
 
-# SQL Server Database credentials and connection details
-DATABASE_URL = "mssql+pyodbc://sa:MySecretPassw0rd!@mssql_db:1433/master?driver=ODBC+Driver+17+for+SQL+Server"
-engine = create_engine(DATABASE_URL)
+    try:
+        with engine.connect() as connection:
+            data_to_insert = {
+                "id": 1,
+                "name": "Jane Doe",
+                "position": "Data Engineer",
+                "database_url": database_url,
+            }
+            connection.execute(employees.insert(), data_to_insert)
+            connection.commit()
+            print(f"Data inserted successfully into {connection.engine} ")
+    except SQLAlchemyError as e:
+        print(f"An error occurred: {e}")
 
-metadata = MetaData()
 
-# Define the table
-employees = Table(
-    "employees",
-    metadata,
-    Column("id", Integer, primary_key=True),
-    Column("name", String),
-    Column("position", String),
-    Column("database_url", String),
-)
+DATABASE_URLS = [
+    "postgresql+psycopg2://postgres:mysecretpassword@postgres_db:5432/postgres",
+    "mssql+pyodbc://sa:MySecretPassw0rd!@mssql_db:1433/master?driver=ODBC+Driver+17+for+SQL+Server",
+]
 
-# Create the table in the database
-metadata.create_all(engine)
-
-try:
-    with engine.connect() as connection:
-        # Insert data
-        data_to_insert = {
-            "id": 1,
-            "name": "Jane Doe",
-            "position": "Data Engineer",
-            "database_url": DATABASE_URL,
-        }
-        connection.execute(employees.insert(), data_to_insert)
-        connection.commit()
-        print("Data inserted successfully.")
-except SQLAlchemyError as e:
-    print(f"An error occurred: {e}")
+for url in DATABASE_URLS:
+    create_table_and_insert_data(url)
