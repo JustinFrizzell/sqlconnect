c01e91056085f51c07f72a581b0fd748609fe941
Justin Frizzell <143661894+JustinFrizzell@users.noreply.github.com>
2024-01-04 22:54:19 +0000

    Update sqlconnect.yaml structure

--- tests/test_sqlconnect_connector.py
+++ tests/test_sqlconnect_connector.py
@@ -1,11 +1,11 @@
 from sqlconnect import Sqlconnector
 
 CONFIG_DICT = {
-    "sqlalchemy_driver": "mssql+pyodbc",
-    "odbc_driver": "SQL+Server",
-    "server": "dev-server.database.com",
+    "dialect": "mssql",
+    "dbapi": "pyodbc",
+    "host": "dev-server.database.com",
     "database": "DevDB",
-    "options": ["Trusted_Connection=Yes"],
+    "options": {"Trusted_Connection=": "Yes", "driver": "SQL Server"},
 }
 
 
@@ -13,10 +13,7 @@ def test_Sqlconnector_connection_name_with_config_dict():
     # Test that Sqlconnector object connection name is correctly initialised
 
     # Creating an instance of Sqlconnector
-    connector = Sqlconnector("test_connection", config_dict=CONFIG_DICT)
+    connector = Sqlconnector("Database_One", config_dict=CONFIG_DICT)
 
     # Assertions to check if the instance is initialised as expected
-    assert connector.connection_name == "test_connection"
-
-
-# Test the sql functions for errors if given non-strings
+    assert connector.connection_name == "Database_One"
