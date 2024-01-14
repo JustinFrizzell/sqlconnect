fa33a44158a4d263e6e9a735777c8a0934a596a9
Justin Frizzell <143661894+JustinFrizzell@users.noreply.github.com>
2024-01-14 14:09:04 +0000

    Increment version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 setuptools.setup(
     name="sqlconnect",
-    version="0.3.0",
+    version="0.3.1",
     author="Justin Frizzell",
     description="Simplifies connections to SQL databases for data analysts. Populate DataFrames with the results of queries directly from .sql files.",
     long_description=Path("README.md").read_text(encoding="utf-8"),
@@ -14,7 +14,16 @@ setuptools.setup(
         "Source": "https://github.com/JustinFrizzell/sqlconnect",
     },
     packages=setuptools.find_packages(exclude=["tests", "tests.*"]),
-    install_requires=["pandas", "sqlalchemy", "pyyaml", "python-dotenv", "pyodbc", "psycopg2-binary", "oracledb", "PyMySQL"],
+    install_requires=[
+        "pandas",
+        "sqlalchemy",
+        "pyyaml",
+        "python-dotenv",
+        "pyodbc",
+        "psycopg2-binary",
+        "oracledb",
+        "PyMySQL",
+    ],
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Intended Audience :: Developers",
